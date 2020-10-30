5e56bd28f1a5c2c02660e575c63786117febe90b
Rob Lark <quietcalmrecords@Robs-iMac.local>
2020-10-29 18:28:49 -0700

    completed project

--- users/users-middleware.js
+++ users/users-middleware.js
@@ -1,21 +1,50 @@
-function restrict() {
+const jwt = require("jsonwebtoken");
+
+const roles = ["basic", "admin"];
+
+function restrict(role) {
 	return async (req, res, next) => {
 		try {
-			// express-session will automatically get the session ID from the cookie
-			// header, and check to make sure it's valid and the session for this user exists.
-			if (!req.session || !req.session.user) {
+			//get the token from a cookie which is automatically sent from the client
+			const token = req.cookies.token;
+			if (!token) {
 				return res.status(401).json({
 					message: "Invalid credentials",
-				})
+				});
 			}
 
-			next()
-		} catch(err) {
-			next(err)
+			//make sure the signature on the token is valid and still mathces the payload
+			//we need to use the same secret string that was used to sign the token
+			jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+				if (err) {
+					return res.status(401).json({
+						message: "Invalid credentials",
+					});
+				}
+
+				// if (role && roles.indexOf(decoded.userRole) < roles.indexOf(role)) {
+				// 	return res.status(401).json({
+				// 		message: "Invalid credentials",
+				// 	});
+				// }
+				// make the token's decoded payload available to other mifflare functions or route handlers in case we want to use it later on
+				req.token = decoded;
+				console.log(decoded)
+				if ((decoded.userRole === 'basic')) {
+					return res.status(401).json({
+						message: "You do not have clearence",
+					});
+				}
+
+				//at this point we know the token and the user is authorized
+				next();
+			});
+		} catch (err) {
+			next(err);
 		}
-	}
+	};
 }
 
 module.exports = {
 	restrict,
-}
+};
--- users/users-router.js
+++ users/users-router.js
@@ -1,72 +1,83 @@
-const express = require("express")
-const bcrypt = require("bcryptjs")
-const Users = require("./users-model")
-const { restrict } = require("./users-middleware")
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const Users = require("./users-model");
+const jwt = require("jsonwebtoken");
+const { restrict } = require("./users-middleware");
 
-const router = express.Router()
+const router = express.Router();
 
 router.get("/users", restrict(), async (req, res, next) => {
 	try {
-		res.json(await Users.find())
-	} catch(err) {
-		next(err)
+		res.json(await Users.find());
+	} catch (err) {
+		next(err);
 	}
-})
+});
 
 router.post("/users", async (req, res, next) => {
 	try {
-		const { username, password } = req.body
-		const user = await Users.findByUsername(username)
+		const { username, password } = req.body;
+		const user = await Users.findByUsername(username);
 
 		if (user) {
 			return res.status(409).json({
 				message: "Username is already taken",
-			})
+			});
 		}
 
 		const newUser = await Users.add({
 			username,
 			// hash the password with a time complexity of "14"
 			password: await bcrypt.hash(password, 14),
-		})
+		});
 
-		res.status(201).json(newUser)
-	} catch(err) {
-		next(err)
+		res.status(201).json(newUser);
+	} catch (err) {
+		next(err);
 	}
-})
+});
 
 router.post("/login", async (req, res, next) => {
 	try {
-		const { username, password } = req.body
-		const user = await Users.findByUsername(username)
-		
+		const { username, password } = req.body;
+		const user = await Users.findByUsername(username);
+
 		if (!user) {
 			return res.status(401).json({
 				message: "Invalid Credentials",
-			})
+			});
 		}
 
 		// hash the password again and see if it matches what we have in the database
-		const passwordValid = await bcrypt.compare(password, user.password)
+		const passwordValid = await bcrypt.compare(password, user.password);
 
 		if (!passwordValid) {
 			return res.status(401).json({
 				message: "Invalid Credentials",
-			})
+			});
 		}
 
 		// generate a new session for this user,
 		// and sends back a session ID
-		req.session.user = user
+		// req.session.user = user
+
+		const token = jwt.sign(
+			{
+				userID: user.id,
+				userRole: user.role,
+			},
+			process.env.JWT_SECRET
+		);
+
+		res.cookie("token", token);
 
 		res.json({
 			message: `Welcome ${user.username}!`,
-		})
-	} catch(err) {
-		next(err)
+		});
+	} catch (err) {
+		next(err);
 	}
-})
+});
 
 router.get("/logout", async (req, res, next) => {
 	try {
@@ -75,14 +86,14 @@ router.get("/logout", async (req, res, next) => {
 		// but it becomes useless to them once the session is deleted server-side.
 		req.session.destroy((err) => {
 			if (err) {
-				next(err)
+				next(err);
 			} else {
-				res.status(204).end()
+				res.status(204).end();
 			}
-		})
+		});
 	} catch (err) {
-		next(err)
+		next(err);
 	}
-})
+});
 
-module.exports = router
+module.exports = router;
